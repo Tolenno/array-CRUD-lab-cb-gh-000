a42953787794f20e21fd5d1f939a4dc75232881d
Matt Hough <matt@hough.tv>
2017-09-24 12:44:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element 
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element) #self explanatary 
+  array.unshift(element) #self explanatary
 end
 
 def remove_element_from_end_of_array(array)
@@ -19,15 +19,15 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
+  array.shift #self explanatary
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+  array[index_number] #self explanatary
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
